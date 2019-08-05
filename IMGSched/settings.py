bcf4716d0582dc2eaf3d71011adc1df912850951
Nisarg Patel <nisargmpatel73@gmail.com>
2019-08-05 17:25:21 +0530

    Google Sign-In added

--- IMGSched/settings.py
+++ IMGSched/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'SchedAPI',
+    'social_django',
 ]
 
 MIDDLEWARE = [
@@ -64,6 +65,8 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'social_django.context_processors.backends',
+                'social_django.context_processors.login_redirect',
             ],
         },
     },
--- SchedAPI/views.py
+++ SchedAPI/views.py
@@ -11,7 +11,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
+
 #Create your views here.
+
+
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
